c615a7f65fa586ebb50277fb8e1360dd3fad33e5
Rebecca Blanton <beccaablanton@gmail.com>
2021-03-22 19:05:11 -0700

    finishing footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,8 +6,11 @@ function Footer(){
     <footer className="fixed-bottom footer mt-auto py-3">
         <div className="container">
             <span>
-                <a href="https://github.com/vpickard707/FriendApp" className="fa fa-github"> repo   </a>
-                <a href="https://unsplash.com/" className="fab fa-unsplash"> backgrounds</a>
+                <a href="https://github.com/vpickard707/FriendApp" className="fa fa-github"></a>
+                <a href="https://unsplash.com/" className="fab fa-unsplash"></a>
+                <p className="collaborators">
+                    Collaborators: Rebecca Blanton, Robert Koepp, Joshua Ordaz, Jessica Tax, Violet Pickard and Alex Bachicha
+                </p>
             </span>
         </div>
   </footer>
