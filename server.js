c2730e8fe8f939788c6646720e1c685e1761873e
Rebecca Blanton <beccaablanton@gmail.com>
2021-03-26 21:35:33 -0700

    deploying

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/bffl_db", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   })
   .then(() => {
     console.log("Successfully connected to MongoDB");
